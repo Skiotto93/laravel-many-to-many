451bce0f5e1c7ed791fcf51aabee12d574180ea8
Skiotto93 <108682070+Skiotto93@users.noreply.github.com>
2023-02-02 16:07:16 +0100

    Initial commit

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        Project::truncate();
+
+        for($i = 0; $i < 5; $i++) {
+
+            $type = Type::inRandomOrder()->first();
+
+            $new_project = new Project();
+    
+            $new_project->name = $faker->sentence(2);
+            $new_project->description = $faker->text(1000);
+            $new_project->name_client = $faker->sentence();
+            $new_project->slug = Str::slug($new_project->name, '-');
+            $new_project->types_id = $type->id;
+    
+            $new_project->save();
+        }
+    }
+}
---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="py-3">
+            <span>
+                <strong>Nome Progetto:</strong> {{$project->name}}
+            </span>
+            <p class="my-3">
+                <strong>Descrizione: </strong>{{$project->description}}
+            </p>
+        </div>
+        <div>
+            @if ($project->cover_image)
+            <div>
+                <strong>Immagine: </strong>
+            </div>
+            <div class="d-flex justify-content-center">
+                <img class="my-4 img-thumbnail img-fluid" src="{{asset('storage/' . $project->cover_image)}}" alt="{{$project->name}}">
+            </div>
+            @endif
+        </div>
+        <div class="mb-3">
+            @if ($project->types_id)
+            <strong>Livello complessità: {{$project->types_id}}</strong>
+            @else
+            <p>Nessuna complessità selezionata</p>
+            @endif
+        </div>
+        <div class="d-flex justify-content-center">
+            <a href="{{ route('admin.projects.index') }}" class="btn btn-primary">Torna alla Dashboard</a>
+        </div>
+    </div>
+@endsection
